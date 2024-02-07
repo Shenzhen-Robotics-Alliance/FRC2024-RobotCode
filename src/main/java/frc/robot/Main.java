7dbae040f414a109c261ecc2fdfa815f86c30b1a
xuzimingtom <xuzimingtom@outlook.com>
2024-02-07 15:36:18 +0800

    Started the annotation & learning process from main. It's gonna be very hard & time-consuming given my bad knowledge of java language and the wpilib. Wish to finish in two weeks, will update a full plan later.

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -11,6 +11,10 @@ import edu.wpi.first.wpilibj.RobotBase;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("@@@@@@  void main(String[] args)  %n");
-        RobotBase.startRobot(RobotShell::new);
+        RobotBase.startRobot(RobotShell::new);   // https://www.geeksforgeeks.org/double-colon-operator-in-java/
+        // 可以ctrl点进上面RobotSheLL里面看源码。
+        // 如果是decompiled的源码，intellij
+        // idea有下载源文件选项（不知道vscode有没有）。
+        // 下了源文件可以看库里面的javadoc
     }
 }
